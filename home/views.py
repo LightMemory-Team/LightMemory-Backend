86a1b19b2a815b540b2e63b68dc4ca6d333be1a3
vevila <s11144225@gmail.com>
2026-08-20 19:57:46 +0800

    更新: 增加使用者查詢與每日建議的錯誤處理，新增 service.py 文件

--- home/views.py
+++ home/views.py
@@ -5,7 +5,9 @@ from games.models import Game
 from datetime import date
 
 # Create your views here.
-
+'''
+目前的建議跟問候僅作api 連接測試，之後放入service.py
+'''
 # 首頁連線
 @api_view(['GET'])
 def ping(request):
@@ -14,8 +16,10 @@ def ping(request):
 # 使用者名稱 != username
 @api_view(['GET'])
 def get_user(request):
-    user_nam = User.objects.first() # 目前還沒有登入驗證先取資料庫第一筆
-    return Response({"user_name": user_nam.name})
+    user_name = User.objects.first() # 目前還沒有登入驗證先取資料庫第一筆
+    if user_name is None:
+        return Response({"user_name_error": "查無使用者"}, status=404)
+    return Response({"user_name": user_name.name})
 
 # 問候語
 @api_view(['GET'])
@@ -23,6 +27,8 @@ def greet(request):
     tips = ["今天也要保持大腦活力喔。", "早安,今天心情如何呢？", "陽光正好,適合散散步。"]
     today = date.today()
     index = today.toordinal() % len(tips)
+    if tips is None:
+            return Response({"daily_tip_error": "查無今日問安"}, status=404)
     return Response({"daily_tip": tips[index]})
 
 # 每日建議
@@ -32,6 +38,8 @@ def daily_suggetion(request):
         "text": "完成一場菜市場遊戲",
         "action_route": "game_market_sort"
         }
+    if daily_suggestion is None:
+            return Response({"daily_suggetion_error": "查無建議事項"}, status=404)
     return Response({"daily_suggestion": daily_suggestion})
 
 # 遊戲卡片列表
